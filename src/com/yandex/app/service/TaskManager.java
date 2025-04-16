4f1e9523ee00a09db39dc5c22520928a88340ad2
ElmoCrumley <Elmo.Crumley@yandex.ru>
2025-04-16 18:56:35 +0300

    Исправить стиль.

--- src/com/yandex/app/service/TaskManager.java
+++ src/com/yandex/app/service/TaskManager.java
@@ -9,30 +9,41 @@ import java.util.ArrayList;
 public interface TaskManager {
     // Получение списка всех задач.
     ArrayList<Task> getTasksList();
+
     ArrayList<Epic> getEpicsList();
+
     ArrayList<SubTask> getSubTasksList();
 
     // Удаление всех задач.
     void removeTasks();
+
     void removeEpics();
+
     void removeSubTasks();
 
     // Получение по идентификатору.
     Task getTask(int id);
+
     Epic getEpic(int id);
+
     SubTask getSubTask(int id);
 
     // Создание.
     void createTask(Task task);
+
     void createEpic(Epic epic);
+
     void createSubTask(SubTask subTask, int epicsId);
 
     // Обновление.
     void updateTask(Task task);
+
     void updateSubTask(SubTask subTask);
 
     // Удаление по идентификатору.
     void removeTask(int id);
+
     void removeEpic(int id);
+
     void removeSubTask(int id);
 }
