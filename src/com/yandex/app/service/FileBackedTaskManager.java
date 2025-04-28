d7277cec2d62fc4f269f85bb2479eb3f4210fd49
ElmoCrumley <Elmo.Crumley@yandex.ru>
2025-04-28 19:00:21 +0300

    Доработал ещё немного тест.

--- src/com/yandex/app/service/FileBackedTaskManager.java
+++ src/com/yandex/app/service/FileBackedTaskManager.java
@@ -10,7 +10,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager implements TaskMa
     static File autoSave;
 
     public FileBackedTaskManager(File autoSave) {
-        this.autoSave = autoSave;
+        FileBackedTaskManager.autoSave = autoSave;
     }
 
     @Override
--- test/com/yandex/app/service/FileBackedTaskManagerTest.java
+++ test/com/yandex/app/service/FileBackedTaskManagerTest.java
@@ -1,9 +1,12 @@
 package com.yandex.app.service;
 
+import com.yandex.app.model.Epic;
+import com.yandex.app.model.SubTask;
 import com.yandex.app.model.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.*;
+import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileBackedTaskManagerTest {
@@ -12,15 +15,19 @@ class FileBackedTaskManagerTest {
     Task task3;
     File log;
     TaskManager fileBackedtaskManager;
+    TaskManager fileBackedtaskManager2;
+
 
     @BeforeEach
-    void beforeEach() {
+    void beforeEach() throws IOException {
         task = new Task("Test addNewTask", "Test addNewTask description");
         task2 = new Task("Test addNewTask", "Test addNewTask description");
         task3 = new Task("Test addNewTask", "Test addNewTask description");
 
         try {
             log = File.createTempFile("myTempFile", ".txt");
+            log = File.createTempFile("myTempFile2", ".txt");
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -31,6 +38,28 @@ class FileBackedTaskManagerTest {
     @Test
     void createTask() throws IOException {
         assertNotNull(log);
+
+        fileBackedtaskManager.createTask(task);
+        fileBackedtaskManager.createTask(task2);
+        fileBackedtaskManager.createTask(task3);
+        System.out.println(fileBackedtaskManager.getTasksList());
+
+        Reader fileReader = new FileReader(log.getName());
+        BufferedReader br = new BufferedReader(fileReader);
+        while (br.ready()) {
+            String split = br.readLine();
+            System.out.println(split);
+        }
+        fileReader.close();
+        br.close();
+
+//        try {
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+        fileBackedtaskManager2 = FileBackedTaskManager.loadFromFile(log);
+        ArrayList<Task> tasks = fileBackedtaskManager2.getTasksList();
+
 
 
     }
