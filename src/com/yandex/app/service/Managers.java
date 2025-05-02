e59c0b4835f2f0c866a3802b6d51c8e0ad6aa140
PersonGit <MW826124@gmail.com>
2025-05-02 22:37:00 +0300

    Удалить лишний метод, изменить изначальный метод передачи объекта менеджера задач.

--- src/com/yandex/app/service/Managers.java
+++ src/com/yandex/app/service/Managers.java
@@ -6,16 +6,12 @@ import com.yandex.app.model.Task;
 import java.io.File;
 
 public class Managers {
-    public static TaskManager getDefault() {
-        return new InMemoryTaskManager();
+    public static TaskManager getDefault(File file) {
+        return FileBackedTaskManager.loadFromFile(file);
     }
 
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
-    }
-
-    public static TaskManager getDefaultSave(File file) {
-        return FileBackedTaskManager.loadFromFile(file);
     }
 
     public static void printAllTasks(TaskManager manager) {
