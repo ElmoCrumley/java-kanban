8bcdaa6c6bdcf36b58fca3b4aa3d6ff811f94059
PersonGit <MW826124@gmail.com>
2025-05-03 17:11:49 +0300

    Удалить неиспользуемые импорты.

--- src/com/yandex/app/Main.java
+++ src/com/yandex/app/Main.java
@@ -1,10 +1,5 @@
 package com.yandex.app;
 
-import com.yandex.app.model.Epic;
-import com.yandex.app.model.SubTask;
-import com.yandex.app.model.Task;
-import com.yandex.app.service.Managers;
-
 public class Main {
 
     public static void main(String[] args) {
