c173bf35b26a28d1f7d001f93ffc82f2d1f397a1
PersonGit <MW826124@gmail.com>
2025-05-27 10:54:57 +0300

    Продолжить дополнять класс TaskManagerTest.

--- test/com/yandex/app/service/TaskManagerTest.java
+++ test/com/yandex/app/service/TaskManagerTest.java
@@ -4,37 +4,17 @@ import com.yandex.app.model.Task;
 import com.yandex.app.model.Epic;
 import com.yandex.app.model.SubTask;
 import com.yandex.app.model.TasksForTests;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
-import java.io.IOException;
 import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public abstract class TaskManagerTest<T extends TaskManager> {
     T taskManager;
     TasksForTests tft;
 
-    @BeforeEach
-    public void beforeEach() throws IOException {
-        tft = new TasksForTests();
-    }
-
     @Test
     public void testMultipleAssertionsWithStreamForCreateAnyTask() {
-        for (Task task : tft.tasks) {
-            taskManager.createTask(task);
-        }
-        for (Epic epic : tft.epics) {
-            taskManager.createEpic(epic);
-        }
-        for (int i = 0; i < tft.allSubtasks.size(); i++) {
-            for (int j = 0; j < tft.allSubtasks.get(i).size(); j++) {
-                taskManager.createSubTask(tft.allSubtasks.get(i).get(j), tft.epics.get(i).getId());
-            }
-        }
-
         Task copyOfTask1 = tft.task1;
         Epic copyOfEpic1 = tft.epic1;
         SubTask copyOfSubtask1 = tft.subTask1d1;
@@ -53,18 +33,6 @@ public abstract class TaskManagerTest<T extends TaskManager> {
 
     @Test
     public void testMultipleAssertionsWithStreamForGetList() {
-        for (Task task : tft.tasks) {
-            taskManager.createTask(task);
-        }
-        for (Epic epic : tft.epics) {
-            taskManager.createEpic(epic);
-        }
-        for (int i = 0; i < tft.allSubtasks.size(); i++) {
-            for (int j = 0; j < tft.allSubtasks.get(i).size(); j++) {
-                taskManager.createSubTask(tft.allSubtasks.get(i).get(j), tft.epics.get(i).getId());
-            }
-        }
-
         int tasksSize = taskManager.getTasksList().size();
         int epicsSize = taskManager.getEpicsList().size();
         int subTasksSize = taskManager.getSubTasksList().size();
@@ -79,50 +47,30 @@ public abstract class TaskManagerTest<T extends TaskManager> {
     }
 
     @Test
-    public void testMultipleAssertionsWithStreamForRemove() {
-        for (Task task : tft.tasks) {
-            taskManager.createTask(task);
-        }
-        for (Epic epic : tft.epics) {
-            taskManager.createEpic(epic);
-        }
-        for (int i = 0; i < tft.allSubtasks.size(); i++) {
-            for (int j = 0; j < tft.allSubtasks.get(i).size(); j++) {
-                taskManager.createSubTask(tft.allSubtasks.get(i).get(j), tft.epics.get(i).getId());
-            }
-        }
+    public void testMultipleAssertionsWithStreamForRemoveAllTask() {
+        int task1Id = tft.task1.getId();
+        int epic2Id = tft.epic2.getId();
+        int subTask1d1Id = tft.subTask1d1.getId();
 
-        taskManager.removeTask(tft.task1.getId());
-        taskManager.removeEpic(tft.epic1.getId());
-        taskManager.removeSubTask(tft.subTask2d1.getId());
+        int TasksCount = taskManager.getTasksList().size();
+        int EpicsCount = taskManager.getTasksList().size();
+        int SubTasksCount = taskManager.getTasksList().size();
 
-        int tasksSize = taskManager.getTasksList().size();
-        int epicsSize = taskManager.getEpicsList().size();
-        int subTasksSize = taskManager.getSubTasksList().size();
+        taskManager.removeTasks();
+        taskManager.removeEpics();
+        taskManager.removeSubTasks();
 
         Stream<Executable> executables = Stream.of(
-                () -> assertEquals(2, tasksSize),
-                () -> assertEquals(2, epicsSize),
-                () -> assertEquals(5, subTasksSize) // Учитывать удалённый эпик, в котором 3 подзадачи
+                () -> assertNotEquals(taskManager.getTasksList().size(), TasksCount),
+                () -> assertNotEquals(taskManager.getEpicsList().size(), EpicsCount),
+                () -> assertNotEquals(taskManager.getSubTasksList().size(), SubTasksCount)
         );
 
-        assertAll("Checking remove tests for Tasks, Epics, Subtasks", executables);
+        assertAll("Checking remove all tasks tests for Tasks, Epics, Subtasks", executables);
     }
 
     @Test
     public void testMultipleAssertionsWithStreamForGetAnyTask() {
-        for (Task task : tft.tasks) {
-            taskManager.createTask(task);
-        }
-        for (Epic epic : tft.epics) {
-            taskManager.createEpic(epic);
-        }
-        for (int i = 0; i < tft.allSubtasks.size(); i++) {
-            for (int j = 0; j < tft.allSubtasks.get(i).size(); j++) {
-                taskManager.createSubTask(tft.allSubtasks.get(i).get(j), tft.epics.get(i).getId());
-            }
-        }
-
         Task copyOfTask1 = taskManager.getTask(tft.task1.getId());
         Epic copyOfEpic1 = taskManager.getEpic(tft.epic1.getId());
         SubTask copyOfSubtask1 = taskManager.getSubTask(tft.subTask1d1.getId());
@@ -138,18 +86,6 @@ public abstract class TaskManagerTest<T extends TaskManager> {
 
     @Test
     public void testMultipleAssertionsWithStreamForUpdateAnyTask() {
-        for (Task task : tft.tasks) {
-            taskManager.createTask(task);
-        }
-        for (Epic epic : tft.epics) {
-            taskManager.createEpic(epic);
-        }
-        for (int i = 0; i < tft.allSubtasks.size(); i++) {
-            for (int j = 0; j < tft.allSubtasks.get(i).size(); j++) {
-                taskManager.createSubTask(tft.allSubtasks.get(i).get(j), tft.epics.get(i).getId());
-            }
-        }
-
         Task fixedTask1 = taskManager.getTask(tft.task1.getId());
         Epic fixedEpic1 = taskManager.getEpic(tft.epic1.getId());
         SubTask fixedSubTask1 = taskManager.getSubTask(tft.subTask1d1.getId());
@@ -163,48 +99,59 @@ public abstract class TaskManagerTest<T extends TaskManager> {
                 () -> assertNotNull(fixedTask1),
                 () -> assertNotNull(fixedEpic1),
                 () -> assertNotNull(fixedSubTask1),
-                () -> assertNotEquals(tft.task1, fixedTask1),
-                () -> assertNotEquals(tft.epic1, fixedEpic1),
-                () -> assertNotEquals(tft.subTask1d1, fixedSubTask1)
+                () -> assertEquals(tft.task1, fixedTask1),
+                () -> assertEquals(tft.epic1, fixedEpic1),
+                () -> assertEquals(tft.subTask1d1, fixedSubTask1)
+        );
+
+        assertAll("Checking update tests for Tasks, Epics, Subtasks", executables);
+    }
+
+    @Test
+    public void testMultipleAssertionsWithStreamForRemove() {
+        int task1Id = tft.task1.getId();
+        int epic2Id = tft.epic2.getId();
+        int subTask1d1Id = tft.subTask1d1.getId();
+
+        taskManager.removeTask(task1Id);
+        taskManager.removeEpic(epic2Id);
+        taskManager.removeSubTask(subTask1d1Id);
+
+        int tasksSize = taskManager.getTasksList().size();
+        int epicsSize = taskManager.getEpicsList().size();
+        int subTasksSize = taskManager.getSubTasksList().size();
+
+        Stream<Executable> executables = Stream.of(
+                () -> assertNull(taskManager.getTask(task1Id)),
+                () -> assertNull(taskManager.getEpic(epic2Id)),
+                () -> assertNull(taskManager.getSubTask(subTask1d1Id)),
+                () -> assertEquals(2, tasksSize),
+                () -> assertEquals(2, epicsSize),
+                () -> assertEquals(5, subTasksSize) // Учитывать удалённый эпик, в котором 3 подзадачи
+        );
+
+        assertAll("Checking remove tests for Tasks, Epics, Subtasks", executables);
+    }
+
+    @Test
+    public void testMultipleAssertionsWithStreamForClearAllTasks() {
+        int TasksCount = taskManager.getTasksList().size();
+        int EpicsCount = taskManager.getTasksList().size();
+        int SubTasksCount = taskManager.getTasksList().size();
+
+        taskManager.clearAllTasks();
+
+        Stream<Executable> executables = Stream.of(
+                () -> assertNotEquals(taskManager.getTasksList().size(), TasksCount),
+                () -> assertNotEquals(taskManager.getEpicsList().size(), EpicsCount),
+                () -> assertNotEquals(taskManager.getSubTasksList().size(), SubTasksCount)
         );
 
         assertAll("Checking get tests for Tasks, Epics, Subtasks", executables);
     }
-//
-//    // Обновление.
-//    @Test
-//    public void updateTaskTest() {
-//
-//    }
-//
-//    @Test
-//    public void updateSubTaskTest() {
-//
-//    }
-//
-//    // Удаление по идентификатору.
-//    @Test
-//    public void removeTaskTest() {
-//
-//    }
-//
-//    @Test
-//    public void removeEpicTest() {
-//
-//    }
-//
-//    @Test
-//    public void removeSubTaskTest() {
-//
-//    }
-//
-//    @Test
-//    public void clearAllTasksTest() {
-//
-//    }
-//
-//    @Test
-//    public void getHistoryManagerTest() {
-//
+
+//    @Override
+//    public HistoryManager getHistoryManager() {
+//        return historyManager;
 //    }
 }
