95eac2f8567ed22099db64ce3e1a875d507686c2
ElmoCrumley <Elmo.Crumley@yandex.ru>
2025-05-28 18:48:17 +0300

    Начать создавать класс HistoryManagerTest.

---
+++ test/com/yandex/app/service/HistoryManagerTest.java
@@ -0,0 +1,17 @@
+package com.yandex.app.service;
+
+import com.yandex.app.model.Task;
+
+import java.util.List;
+
+public class HistoryManagerTest {
+//    public void addTask(Task task);
+//
+//    public void remove(int id);
+//
+//    public List<Task> getHistory();
+//
+//    public void clearAllHistory();
+//
+//    public boolean historyIsEmpty();
+}
