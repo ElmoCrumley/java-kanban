a3563454e7940baa403aa0643c73a8889103f060
PersonGit <MW826124@gmail.com>
2025-05-29 08:06:51 +0300

    Доделать класс InMemoryTaskManagerTest.

--- test/com/yandex/app/service/InMemoryTaskManagerTest.java
+++ test/com/yandex/app/service/InMemoryTaskManagerTest.java
@@ -1,10 +1,17 @@
 package com.yandex.app.service;
 
 import com.yandex.app.model.Epic;
+import com.yandex.app.model.SubTask;
 import com.yandex.app.model.Task;
 import com.yandex.app.model.TasksForTests;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager> {
     @BeforeEach
@@ -65,45 +72,89 @@ public class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager
         testForGetHistoryManager();
     }
 
-//    public Map<Integer, Task> getTasks() {
-//        return tasks;
-//    }
-//
-//    public Map<Integer, Epic> getEpics() {
-//        return epics;
-//    }
-//
-//    public Map<Integer, SubTask> getSubTasks() {
-//        return subTasks;
-//    }
-//
-//    // Получение списка всех подзадач определённого эпика. (дополнительный метод)
-//    public static ArrayList<SubTask> getEpicsSubTasksList(Epic epic) {
-//        return new ArrayList<>(epic.getSubTasksList());
-//    }
-//
-//    public ArrayList<Task> getPrioritizedTasks() {
-//        return new ArrayList<>(allTasksWithDuration);
-//    }
-//
-//    public boolean isNotIntersectAny(Task t2) {
-//        if (!allTasksWithDuration.isEmpty()) {
-//            return (allTasksWithDuration.stream().filter(task -> task.isIntersect(t2)).count() == 0);
-//        } else{
-//            return true;
-//        }
-//    }
-//
-//    public static class DataComparator implements Comparator<Task> {
-//        @Override
-//        public int compare(Task o1, Task o2) {
-//            if (o1.getStartTime().isBefore(o2.getStartTime())) {
-//                return -1;
-//            } else if (o1.getStartTime().isAfter(o2.getStartTime())) {
-//                return 1;
-//            } else {
-//                return 0;
-//            }
-//        }
-//    }
+    @Test
+    public void getTasksEpicsSubTasksTests() {
+        Map<Integer, Task> tasks = taskManager.getTasks();
+        Map<Integer, Epic> epics = taskManager.getEpics();
+        Map<Integer, SubTask> subTasks = taskManager.getSubTasks();
+
+        Stream<Executable> executables = Stream.of(
+                () -> assertNotNull(tasks),
+                () -> assertNotNull(epics),
+                () -> assertNotNull(subTasks),
+                () -> assertEquals(tasks, taskManager.getTasks()),
+                () -> assertEquals(epics, taskManager.getEpics()),
+                () -> assertEquals(subTasks, taskManager.getSubTasks())
+        );
+
+        assertAll("Checking getMap tests for Tasks, Epics, Subtasks", executables);
+    }
+
+    @Test
+    public void getEpicsSubTasksListTest() {
+        ArrayList<SubTask> subTasksList1 = InMemoryTaskManager.getEpicsSubTasksList(tft.epic1);
+
+        Stream<Executable> executables = Stream.of(
+                () -> assertNotNull(subTasksList1),
+                () -> assertEquals(subTasksList1, InMemoryTaskManager.getEpicsSubTasksList(tft.epic1)),
+                () -> assertEquals(3, subTasksList1.size())
+        );
+
+        assertAll("Checking getEpicsSubTasksList test for Epics", executables);
+    }
+
+    @Test
+    public void dataComparatorTest() {
+        Task task1 = tft.task1;
+        Task task2 = tft.task2;
+        Task task3 = tft.task3;
+
+        task1.setStartTime(LocalDateTime.of(2025, 1, 1, 11, 0)); // 00 -> 20
+        task1.setDuration(20);
+        task2.setStartTime(LocalDateTime.of(2025, 1, 1, 11, 15)); // 15 -> 35
+        task2.setDuration(20);
+        task3.setStartTime(LocalDateTime.of(2025, 1, 1, 11, 21)); // 21 -> 41
+        task3.setDuration(20);
+
+        Stream<Executable> executables = Stream.of(
+                () -> assertFalse(task1.isIntersect(task3)),
+                () -> assertTrue(task2.isIntersect(task1)),
+                () -> assertTrue(task2.isIntersect(task3))
+        );
+
+        assertAll("Checking dataComparator test", executables);
+    }
+
+    @Test
+    public void getPrioritizedTasksTest() {
+        Task task1 = new Task("Test task1", "Test task1 description");
+        Task task2 = new Task("Test task2", "Test task2 description");
+        Task task3 = new Task("Test task3", "Test task3 description");
+
+        task1.setStartTime(LocalDateTime.of(2025, 1, 1, 11, 50)); // 00 -> 20
+        task1.setDuration(10);
+        task2.setStartTime(LocalDateTime.of(2025, 1, 1, 11, 30)); // 15 -> 35
+        task2.setDuration(10);
+        task3.setStartTime(LocalDateTime.of(2025, 1, 1, 11, 10)); // 21 -> 41
+        task3.setDuration(10);
+        taskManager.createTask(task1);
+        taskManager.createTask(task2);
+        taskManager.createTask(task3);
+
+        ArrayList<Task> prioritizedTasks = taskManager.getPrioritizedTasks();
+
+        Task taskFirst = prioritizedTasks.get(0);
+        Task taskASecond = prioritizedTasks.get(1);
+        Task taskThird = prioritizedTasks.get(2);
+
+        Stream<Executable> executables = Stream.of(
+                () -> assertNotNull(prioritizedTasks),
+                () -> assertEquals(3, prioritizedTasks.size()),
+                () -> assertEquals(taskFirst, task3),
+                () -> assertEquals(taskASecond, task2),
+                () -> assertEquals(taskThird, task1)
+        );
+
+        assertAll("Checking dataComparator test", executables);
+    }
 }
