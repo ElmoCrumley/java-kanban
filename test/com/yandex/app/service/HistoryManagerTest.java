8fec119bf2ba90ee4de6b521c2c4df9a20ec9d72
ElmoCrumley <Elmo.Crumley@yandex.ru>
2025-05-29 18:46:56 +0300

    Изменить сигнатуру, добавить поля.

--- test/com/yandex/app/service/HistoryManagerTest.java
+++ test/com/yandex/app/service/HistoryManagerTest.java
@@ -1,10 +1,10 @@
 package com.yandex.app.service;
 
-import com.yandex.app.model.Task;
+import com.yandex.app.model.TasksForTests;
 
-import java.util.List;
-
-public class HistoryManagerTest {
+public class HistoryManagerTest<T extends HistoryManager> {
+    T historyManager;
+    TasksForTests tft;
 //    public void addTask(Task task);
 //
 //    public void remove(int id);
