f550866afa89d7200a0aaf6a181c4b5623b38d1e
ElmoCrumley <Elmo.Crumley@yandex.ru>
2025-05-29 18:47:09 +0300

    Кодстайл.

--- test/com/yandex/app/service/InMemoryHistoryManagerTest.java
+++ test/com/yandex/app/service/InMemoryHistoryManagerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
