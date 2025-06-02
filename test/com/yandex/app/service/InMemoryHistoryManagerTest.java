4c3b7d493448dc15c01b611c9141b80db56f3878
PersonGit <MW826124@gmail.com>
2025-06-02 08:55:20 +0300

    Переделать класс InMemoryHistoryManagerTest, чтобы он вызывал методы HistoryManagerTest.

--- test/com/yandex/app/service/InMemoryHistoryManagerTest.java
+++ test/com/yandex/app/service/InMemoryHistoryManagerTest.java
@@ -1,82 +1,42 @@
 package com.yandex.app.service;
+import com.yandex.app.model.Epic;
 import com.yandex.app.model.Task;
+import com.yandex.app.model.TasksForTests;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
-import static org.junit.jupiter.api.Assertions.*;
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
 
-class InMemoryHistoryManagerTest {
-    Task task;
-    Task task2;
-    Task task3;
-    File log;
-    TaskManager taskManager;
-    HistoryManager historyManager;
-
+class InMemoryHistoryManagerTest extends HistoryManagerTest {
     @BeforeEach
-    void beforeEach() {
-        try {
-            log = File.createTempFile("myTempFile", ".txt");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        taskManager = Managers.getDefault(log);
+    void setUp() {
+        taskManager = new InMemoryTaskManager();
         historyManager = taskManager.getHistoryManager();
+        tft = new TasksForTests();
 
-        task = new Task("Test addNewTask", "Test addNewTask description");
-        task2 = new Task("Test addNewTask", "Test addNewTask description");
-        task3 = new Task("Test addNewTask", "Test addNewTask description");
-        taskManager.createTask(task);
-        taskManager.createTask(task2);
-        taskManager.createTask(task3);
-    }
-
-    @AfterEach
-    void afterEach() {
-        taskManager.clearAllTasks();
-        historyManager.clearAllHistory();
-    }
-
-    @Test
-    void addTask() {
-        historyManager.addTask(task);
-
-        List<Task> history = historyManager.getHistory();
-
-        assertNotNull(history);
-        assertEquals(1, history.size());
-        historyManager.addTask(task2);
-        history = historyManager.getHistory();
-        assertNotNull(history);
-        assertEquals(2, history.size());
+        for (Task task : tft.tasks) {
+            taskManager.createTask(task);
+        }
+        for (Epic epic : tft.epics) {
+            taskManager.createEpic(epic);
+        }
+        for (int i = 0; i < tft.allSubtasks.size(); i++) {
+            for (int j = 0; j < tft.allSubtasks.get(i).size(); j++) {
+                taskManager.createSubTask(tft.allSubtasks.get(i).get(j), tft.epics.get(i).getId());
+            }
+        }
     }
 
     @Test
-    void remove() {
-        historyManager.addTask(task);
-        historyManager.remove(task.getId());
-
-        final List<Task> history = historyManager.getHistory();
-        int historySize = history.size();
-
-        assertEquals(0, historySize);
+    public void addTaskTest() {
+        testForAddTask();
     }
 
     @Test
-    void getHistory() {
-        List<Task> history;
+    public void removeTest() {
+        testForRemove();
+    }
 
-        historyManager.addTask(task);
-        history = historyManager.getHistory();
-        assertEquals(1, history.size());
-        historyManager.addTask(task2);
-        history = historyManager.getHistory();
-        assertEquals(2, history.size());
-        historyManager.addTask(task3);
-        history = historyManager.getHistory();
-        assertEquals(3, history.size());
+    @Test
+    public void getHistoryTest() {
+        testForGetHistory();
     }
 }
