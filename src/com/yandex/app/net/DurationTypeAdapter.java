9bebbe8980904e269ba008470e7ecd20ee182039
ElmoCrumley <MW826124@gmail.com>
2025-07-15 10:51:01 +0300

    Создать DurationTypeAdapter для сериализации/десериализации продолжительности.

---
+++ src/com/yandex/app/net/DurationTypeAdapter.java
@@ -0,0 +1,22 @@
+package com.yandex.app.net;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.time.Duration;
+
+public class DurationTypeAdapter extends TypeAdapter<Duration> {
+
+    @Override
+    public void write(JsonWriter out, Duration duration) throws IOException {
+        out.value(duration.getSeconds());
+    }
+
+    @Override
+    public Duration read(JsonReader in) throws IOException {
+        long seconds = in.nextLong();
+        return Duration.ofSeconds(seconds);
+    }
+}
