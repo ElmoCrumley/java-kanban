b4ddedad0a772e72a51efe0bc14106cc2b002fbf
ElmoCrumley <MW826124@gmail.com>
2025-07-20 18:39:59 +0300

    Добавить бросок исключения, вывод в консоль код.

--- src/com/yandex/app/net/BaseHttpHandler.java
+++ src/com/yandex/app/net/BaseHttpHandler.java
@@ -3,35 +3,50 @@ package com.yandex.app.net;
 import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 class BaseHttpHandler {
     protected void sendText(HttpExchange h, String text) throws IOException {
-        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
-        h.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
-        h.sendResponseHeaders(200, resp.length);
-        h.getResponseBody().write(resp);
-        h.close();
+        try {
+            byte[] resp = text.getBytes(); //StandardCharsets.UTF_8
+            h.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+            h.sendResponseHeaders(200, resp.length);
+            System.out.println("Запрос /" + h.getRequestURI().toString().split("/")[1] + ", код 200" + "\n");
+            h.getResponseBody().write(resp);
+            h.close();
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace());
+        }
     }
 
     protected void sendText(HttpExchange h, int code) throws IOException {
+        System.out.println("Запрос /" + h.getRequestURI().toString().split("/")[1] + ", код " + code + "\n");
         h.sendResponseHeaders(code, 0);
         h.close();
     }
 
     protected void sendNotFound(HttpExchange h) throws IOException {
-        byte[] resp = "Not Found".getBytes(StandardCharsets.UTF_8);
-        h.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
-        h.sendResponseHeaders(404, resp.length);
-        h.getResponseBody().write(resp);
-        h.close();
+        try {
+            byte[] resp = "Not Found".getBytes(); //StandardCharsets.UTF_8
+            h.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+            h.sendResponseHeaders(404, resp.length);
+            System.out.println("Запрос /" + h.getRequestURI().toString().split("/")[1] + ", код 404" + "\n");
+            h.getResponseBody().write(resp);
+            h.close();
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace());
+        }
     }
 
     protected void sendHasOverlaps(HttpExchange h) throws IOException {
-        byte[] resp = "Not Acceptable".getBytes(StandardCharsets.UTF_8);
-        h.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
-        h.sendResponseHeaders(406, resp.length);
-        h.getResponseBody().write(resp);
-        h.close();
+        try {
+            byte[] resp = "Not Acceptable".getBytes(); //StandardCharsets.UTF_8
+            h.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+            h.sendResponseHeaders(406, resp.length);
+            System.out.println("Запрос /" + h.getRequestURI().toString().split("/")[1] + ", код 406" + "\n");
+            h.getResponseBody().write(resp);
+            h.close();
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace());
+        }
     }
 }
