482fbb655a2dc9d8892bd4b272806ea274658d15
ElmoCrumley <MW826124@gmail.com>
2025-07-23 17:09:04 +0300

    Удалить неиспользуемые импорты и код в main.

--- src/com/yandex/app/net/HttpTaskServer.java
+++ src/com/yandex/app/net/HttpTaskServer.java
@@ -2,16 +2,10 @@ package com.yandex.app.net;
 
 import com.google.gson.*;
 import com.sun.net.httpserver.HttpServer;
-import com.yandex.app.model.Task;
-import com.yandex.app.service.InMemoryTaskManager;
 import com.yandex.app.service.TaskManager;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
@@ -24,36 +18,7 @@ public class HttpTaskServer {
     }
 
     public static void main(String[] args) throws IOException {
-        try {
-            TaskManager taskManager = new InMemoryTaskManager();
-            HttpTaskServer httpTaskServer = new HttpTaskServer(taskManager);
 
-            httpTaskServer.start();
-//            HttpClient client = HttpClient.newHttpClient();
-//
-//            // Тестовый запрос
-//            HttpRequest request = HttpRequest.newBuilder()
-//                .uri(URI.create("http://localhost:8080/hello"))
-//                .build();
-//
-//            HttpResponse<String> response1 = client.send(request, HttpResponse.BodyHandlers.ofString());
-//
-//            // Post запрос
-//            Gson gson = HttpTaskServer.getGson();
-//            Task task1 = new Task("Name", "Description", Duration.ofMinutes(5), LocalDateTime.now());
-//            String taskJson = gson.toJson(task1);
-//            HttpRequest request = HttpRequest.newBuilder()
-//                    .uri(URI.create("http://localhost:8080/tasks"))
-//                    .header("Content-Type", "application/json")
-//                    .POST(HttpRequest.BodyPublishers.ofString(taskJson))
-//                    .build();
-//
-//            HttpResponse<String> response2 = client.send(request, HttpResponse.BodyHandlers.ofString());
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     public void start() throws IOException {
