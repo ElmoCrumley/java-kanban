ef12b2cc9cf2fabbdaa256a2c67e62764aea8d94
ElmoCrumley <MW826124@gmail.com>
2025-07-23 17:06:23 +0300

    Перенести поля path, id, и gson в try. Добавить вывод списков и одиночных задач. Добавить break. Добавить проверку длины массива из элементов пути. Изменить параметры вызова createSubTask. Изменить проверку в условии с null на 0.

--- src/com/yandex/app/net/SubtasksHandler.java
+++ src/com/yandex/app/net/SubtasksHandler.java
@@ -21,39 +21,64 @@ class SubtasksHandler extends BaseHttpHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        String path = httpExchange.getRequestURI().getPath();
-        String id = path.split("/")[2];
-        Gson gson = HttpTaskServer.getGson();
-
         try {
+            String path = httpExchange.getRequestURI().getPath();
+            String id = null;
+
+            if (path.split("/").length >= 3) {
+                id = path.split("/")[2];
+            }
+
+            Gson gson = HttpTaskServer.getGson();
+
             switch (httpExchange.getRequestMethod()) {
                 case "GET":
                     if (id != null) {
                         SubTask subTask = taskManager.getSubTask(Integer.parseInt(id));
+                        System.out.println(
+                                "Выполнена передача подзадачи: \n" + gson.toJson(subTask)
+                        );
                         sendText(httpExchange, gson.toJson(subTask));
                     } else {
                         ArrayList<SubTask> subTasksList = taskManager.getSubTasksList();
+                        System.out.println("Выполнена передача списка подзадач:");
+                        for (SubTask subTask : subTasksList) {
+                            System.out.println("\"name\": \"" + subTask.getName() + "\", "
+                                    + "\"description\": \"" + subTask.getDescription() + "\";");
+                        }
                         sendText(httpExchange, gson.toJson(subTasksList));
                     }
+                    break;
                 case "POST":
                     // body
                     InputStream inputStream = httpExchange.getRequestBody();
                     String jsonTask = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
                     SubTask subTask = gson.fromJson(jsonTask, SubTask.class);
-                    String epicsId = path.split("/")[3];
 
-                    if (id != null) {
+                    if (subTask.getId() != 0) {
                         taskManager.updateSubTask(subTask);
+                        System.out.println(
+                                "Выполнено обновление подзадачи: \n" + gson.toJson(subTask)
+                        );
                         sendText(httpExchange, 201);
                     } else {
-                        taskManager.createSubTask(subTask, Integer.parseInt(epicsId));
+                        taskManager.createSubTask(subTask, subTask.getEpicsId());
+                        System.out.println(
+                                "Выполнено создание подзадачи: \n" + gson.toJson(subTask)
+                        );
                         sendText(httpExchange, 201);
                     }
+                    break;
                 case "DELETE":
                     if (id != null) {
                         taskManager.removeSubTask(Integer.parseInt(id));
+                        System.out.println(
+                                "Выполнено удаление подзадачи: \n"
+                                        + gson.toJson(taskManager.getSubTask(Integer.parseInt(id)))
+                        );
                         sendText(httpExchange, 200);
                     }
+                    break;
                 default:
                     break;
             }
