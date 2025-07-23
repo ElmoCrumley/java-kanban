a2d8a7a87530e02dff7b9e696bb3240f6cb937c1
ElmoCrumley <MW826124@gmail.com>
2025-07-23 16:49:18 +0300

    Создать TaskListTypeToken.

---
+++ src/com/yandex/app/net/TaskListTypeToken.java
@@ -0,0 +1,9 @@
+package com.yandex.app.net;
+
+import java.util.List;
+import com.yandex.app.model.Task;
+import com.google.gson.reflect.TypeToken;
+
+public class TaskListTypeToken extends TypeToken<List<Task>> {
+
+}
