80c19cfe251d5fcae5b22a9370f96e34bfbfc002
ElmoCrumley <MW826124@gmail.com>
2025-07-23 16:49:03 +0300

    Доделать класс HttpTaskManagerTasksTest.

--- test/com/yandex/app/net/HttpTaskManagerTasksTest.java
+++ test/com/yandex/app/net/HttpTaskManagerTasksTest.java
@@ -4,10 +4,12 @@ import com.google.gson.Gson;
 import com.yandex.app.model.Task;
 import com.yandex.app.model.TasksForTests;
 import com.yandex.app.service.InMemoryTaskManager;
+import com.yandex.app.service.NotFoundException;
+import com.yandex.app.service.Status;
 import com.yandex.app.service.TaskManager;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URI;
@@ -18,15 +20,14 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
-
-class HttpTaskManagerTasksTest {
+public class HttpTaskManagerTasksTest {
     TasksForTests tft;
     TaskManager taskManager = new InMemoryTaskManager();
-    HttpTaskServer httpTaskServer = new HttpTaskServer(taskManager);
     Gson gson = HttpTaskServer.getGson();
+    HttpTaskServer httpTaskServer = new HttpTaskServer(taskManager);
+    HttpClient client = HttpClient.newHttpClient();
 
     @BeforeEach
     public void setUp() throws IOException {
@@ -44,9 +45,13 @@ class HttpTaskManagerTasksTest {
     @Test
     public void tasksPostCreateTaskTest()  throws IOException, InterruptedException  {
         try {
-            HttpClient client = HttpClient.newHttpClient();
+            System.out.println("=".repeat(8) + " Test 1 " + "=".repeat(8));
+
+            // Prepared data
             Task task1 = new Task("Name", "Description", Duration.ofMinutes(5), LocalDateTime.now());
             String taskJson = gson.toJson(task1);
+
+            // POST request
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create("http://localhost:8080/tasks"))
                     .header("Content-Type", "application/json")
@@ -54,13 +59,15 @@ class HttpTaskManagerTasksTest {
                     .build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
+            // Tests
             assertEquals(201, response.statusCode());
 
             List<Task> tasksFromManager = taskManager.getTasksList();
 
             assertNotNull(tasksFromManager, "Задачи не возвращаются");
             assertEquals(1, tasksFromManager.size(), "Некорректное количество задач");
-            assertEquals("Name", tasksFromManager.get(0).getName(), "Некорректное имя задачи");
+            assertEquals("Name", tasksFromManager.getFirst().getName(), "Некорректное имя задачи");
+
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } catch (Exception e) {
@@ -68,83 +75,191 @@ class HttpTaskManagerTasksTest {
         }
     }
 
-//    @Test
-//    void tasksPostUpdateTaskTest() throws IOException, InterruptedException  {
-//
-//    }
-//
-//    @Test
-//    void tasksGetGetTaskTest() throws IOException, InterruptedException  {
-//
-//    }
-//
-//    @Test
-//    void tasksGetGetTasksTest() throws IOException, InterruptedException  {
-//
-//    }
-//
-//    @Test
-//    void tasksDeleteDeleteTaskTest() throws IOException, InterruptedException  {
-//
-//    }
-//
-//    @Test
-//    void subTasksPostCreateSubTaskTest() throws IOException, InterruptedException  {
-//
-//    }
-//
-//    @Test
-//    void subTasksPostUpdateSubTaskTest() throws IOException, InterruptedException  {
-//
-//    }
-//
-//    @Test
-//    void subTasksGetGetSubTaskTest() throws IOException, InterruptedException  {
-//
-//    }
-//
-//    @Test
-//    void subTasksGetGetSubTasksTest() throws IOException, InterruptedException  {
-//
-//    }
-//
-//    @Test
-//    void subTasksDeleteDeleteSubTasksTest() throws IOException, InterruptedException  {
-//
-//    }
-//
-//    @Test
-//    void epicsPostCreateEpicTest() throws IOException, InterruptedException  {
-//
-//    }
-//
-//    @Test
-//    void epicsGetGEtEpicByIdTest() throws IOException, InterruptedException  {
-//
-//    }
-//
-//    @Test
-//    void epicsGetGetEpicsTest() throws IOException, InterruptedException  {
-//
-//    }
-//
-//    @Test
-//    void epicsGetGetEpicSubTasksTest() throws IOException, InterruptedException  {
-//
-//    }
-//
-//    @Test
-//    void epicsDeleteDeleteEpicTest() throws IOException, InterruptedException {
-//
-//    }
-//
-//    @Test
-//    void historyGetGetHistoryTest() throws IOException, InterruptedException  {
-//
-//    }
-//
-//    @Test
-//    void prioritizedGetGetPrioritizedTasksTest() throws IOException, InterruptedException {
-//
-//    }
+    @Test
+    void tasksPostUpdateTaskTest() throws IOException, InterruptedException  {
+        try {
+            System.out.println("=".repeat(8) + " Test 2 " + "=".repeat(8));
+
+            // Prepared data
+            String taskJson;
+            Task task1 = new Task("Name", "Description", Duration.ofMinutes(5), LocalDateTime.now());
+            taskJson = gson.toJson(task1);
+
+            // POST request
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/tasks"))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(taskJson))
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Prepared data
+            task1 = taskManager.getTasksList().getFirst();
+            task1.setStatus(Status.IN_PROGRESS);
+            task1.setDescription("Another description");
+            taskJson = gson.toJson(task1);
+
+            // POST request
+            HttpRequest request2 = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/tasks/293508254"))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(taskJson))
+                    .build();
+            HttpResponse<String> response2 = client.send(request2, HttpResponse.BodyHandlers.ofString());
+
+            // Tests
+            assertEquals(201, response.statusCode());
+
+            List<Task> tasksFromManager = taskManager.getTasksList();
+
+            assertNotNull(tasksFromManager, "Задачи не возвращаются");
+            assertEquals(1, tasksFromManager.size(), "Некорректное количество задач");
+            assertEquals("Another description",
+                    tasksFromManager.getFirst().getDescription(), "Неверное новое описание задачи");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void tasksGetGetTaskTest() throws IOException, InterruptedException  {
+        try {
+            System.out.println("=".repeat(8) + " Test 3 " + "=".repeat(8));
+
+            // Prepared data
+            Task task = tft.task1;
+            String taskJson = gson.toJson(task);
+
+            // POST request
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/tasks"))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(taskJson))
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Prepared data
+            String taskId = String.valueOf(taskManager.getTasksList().getFirst().getId());
+
+            // GET request
+            HttpRequest request2 = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/tasks" + '/' + taskId))
+                    .header("Content-Type", "application/json")
+                    .GET()
+                    .build();
+            HttpResponse<String> response2 = client.send(request2, HttpResponse.BodyHandlers.ofString());
+
+            // Tests
+            Task responseTask = gson.fromJson(response2.body(), Task.class);
+
+            assertEquals(task.getName(), responseTask.getName());
+            assertEquals(task.getDescription(), responseTask.getDescription());
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void tasksGetGetTasksTest() throws IOException, InterruptedException  {
+        try {
+            System.out.println("=".repeat(8) + " Test 4 " + "=".repeat(8));
+
+            int taskCount = 0;
+            for (Task task : tft.tasks) {
+                taskCount++;
+                String taskJson = gson.toJson(task);
+
+                // POST request
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create("http://localhost:8080/tasks"))
+                        .header("Content-Type", "application/json")
+                        .POST(HttpRequest.BodyPublishers.ofString(taskJson))
+                        .build();
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+                // Tests
+                List<Task> tasksFromManager = taskManager.getTasksList();
+
+                assertNotNull(tasksFromManager, "Задачи не возвращаются");
+                assertEquals(taskCount, tasksFromManager.size(), "Некорректное количество задач");
+                System.out.println("taskCount = " + taskCount + "; "
+                        + "tasksFromManager.size() = " + tasksFromManager.size() + "\n");
+
+                // GET request
+                HttpRequest request2 = HttpRequest.newBuilder()
+                        .uri(URI.create("http://localhost:8080/tasks"))
+                        .header("Content-Type", "application/json")
+                        .GET()
+                        .build();
+                HttpResponse<String> response2 = client.send(request2, HttpResponse.BodyHandlers.ofString());
+
+                // Tests
+                List<Task> responseTasksList = gson.fromJson(response2.body(), new TaskListTypeToken().getType());
+
+                assertNotNull(responseTasksList, "Задачи не возвращаются");
+                assertEquals(taskCount, responseTasksList.size(), "Некорректное количество задач");
+            }
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void tasksDeleteDeleteTaskTest() throws IOException, InterruptedException  {
+        boolean thrownTasksCount = false;
+        int tasksCount;
+
+        try {
+            System.out.println("=".repeat(8) + " Test 5 " + "=".repeat(8));
+
+            // Prepared data
+            Task task = tft.task1;
+            String taskJson = gson.toJson(task);
+
+            // POST request
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/tasks"))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(taskJson))
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Prepared data
+            String taskId = String.valueOf(taskManager.getTasksList().getFirst().getId());
+
+            // Tests
+            assertNotNull(taskManager.getTasksList(), "Задачи не возвращаются");
+            assertEquals("Test task1",
+                    taskManager.getTasksList().getFirst().getName(), "Некорректное имя задачи");
+
+            // DELETE request
+            HttpRequest request2 = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/tasks" + '/' + taskId))
+                    .header("Content-Type", "application/json")
+                    .DELETE()
+                    .build();
+            HttpResponse<String> response2 = client.send(request2, HttpResponse.BodyHandlers.ofString());
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Tests
+        try {
+            tasksCount = taskManager.getSubTasksList().size();
+        } catch (NotFoundException e) {
+            thrownTasksCount = true;
+        }
+
+        assertTrue(thrownTasksCount);
+    }
 }
